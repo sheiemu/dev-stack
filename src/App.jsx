6ac6812f0076a8a2fa4723a3c499e50c27b2effb
sheiemu <maimunaemu802@gmail.com>
2026-09-11 02:10:27 -0700

    Build navbar

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div className="logo">
+  
+  <img src="/logo-text.png" alt="Dev Stack" />
+</div>
+
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Technologies</a>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+
+        <div className="nav-buttons">
+          <button>Sign In</button>
+          <button>Sign Up</button>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default App
